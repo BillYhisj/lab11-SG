26e2e9a78735c7b03ac03bafc50150e640b8a6ff
Sebastian Gil <sebastiangil@sebastians.macbook.pro.lan>
2025-11-16 00:06:28 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b): return a + b
+
+def subtract(a, b): return a - b
+
+def multiply(a, b): return a * b
+
+def divide(a, b): # b / a
+    if a == 0:
+        raise ZeroDivisionError("You can not divide a number by zero")
+    else:
+        return b / a
+
+def logarithm(a, b): #loga(b) use math library/raise ValueError
+    if a < 0 or b < 0:
+         raise ValueError("math domain error")
+    else:
+        return math.log(b,a)
+
+def exponent(a, b): return a**b
+
+
 
 
